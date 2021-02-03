133d1c086642d6b2cab125dff9f8601f0c20bb32
Antonio <Antonio@DESKTOP-J8ARSSP.mshome.net>
2021-02-03 13:15:22 +0100

    c2

--- microservicio_ficha_jenkins/src/main/java/controller/FichaController.java
+++ microservicio_ficha_jenkins/src/main/java/controller/FichaController.java
@@ -12,6 +12,9 @@ import model.Ficha;
 public class FichaController {
 	@GetMapping(value="datos/{dni}",produces=MediaType.APPLICATION_JSON_VALUE)
 	public Ficha datosPersona(@PathVariable("dni") String dni) {
+		//transforma la ficha en un JSON
 		return new Ficha("prueba ficha",dni,20);
 	}
+	
+	
 }
