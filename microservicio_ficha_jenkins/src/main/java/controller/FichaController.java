f19ac6935f01c757bcc6dd1fede81255d313dbf3
Antonio <Antonio@DESKTOP-J8ARSSP.mshome.net>
2021-02-03 12:37:03 +0100

    inicio

---
+++ microservicio_ficha_jenkins/src/main/java/controller/FichaController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Ficha;
+//@RequestMapping("sfichas")
+@RestController
+public class FichaController {
+	@GetMapping(value="datos/{dni}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Ficha datosPersona(@PathVariable("dni") String dni) {
+		return new Ficha("prueba ficha",dni,20);
+	}
+}
